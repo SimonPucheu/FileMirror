89bd4dcda922e43d425c1d29ffd71c5469af8667
SimonPucheu <104694344+SimonPucheu@users.noreply.github.com>
2023-04-22 19:00:23 +0200

    Add project files.

---
+++ FileMirror/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace FileMirror
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBoxFileNameSource = new System.Windows.Forms.TextBox();
+            this.buttonBrowseSource = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBoxFileNameTarget = new System.Windows.Forms.TextBox();
+            this.buttonBrowseTarget = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mirrorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialogImport = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialogExport = new System.Windows.Forms.SaveFileDialog();
+            this.folderBrowserDialogSource = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialogTarget = new System.Windows.Forms.FolderBrowserDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.tableLayoutPanel1.SetColumnSpan(this.listView1, 2);
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.tableLayoutPanel1.SetRowSpan(this.listView1, 2);
+            this.listView1.Size = new System.Drawing.Size(314, 391);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.Controls.Add(this.listView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonRemove, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonAdd, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxName, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 3, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 426);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonRemove.Location = new System.Drawing.Point(3, 400);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(154, 23);
+            this.buttonRemove.TabIndex = 1;
+            this.buttonRemove.Text = "-";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.RemoveMirror);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonAdd.Location = new System.Drawing.Point(163, 400);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(154, 23);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "+";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.AddMirror);
+            // 
+            // textBoxName
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBoxName, 2);
+            this.textBoxName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxName.Location = new System.Drawing.Point(323, 3);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(474, 20);
+            this.textBoxName.TabIndex = 3;
+            this.textBoxName.Leave += new System.EventHandler(this.textBoxName_Leave);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel2.Controls.Add(this.textBoxFileNameSource, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonBrowseSource, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(323, 32);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(234, 362);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // textBoxFileNameSource
+            // 
+            this.textBoxFileNameSource.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxFileNameSource.Location = new System.Drawing.Point(3, 3);
+            this.textBoxFileNameSource.Name = "textBoxFileNameSource";
+            this.textBoxFileNameSource.Size = new System.Drawing.Size(199, 20);
+            this.textBoxFileNameSource.TabIndex = 0;
+            this.textBoxFileNameSource.Leave += new System.EventHandler(this.textBoxFileNameSource_Leave);
+            // 
+            // buttonBrowseSource
+            // 
+            this.buttonBrowseSource.Image = ((System.Drawing.Image)(resources.GetObject("buttonBrowseSource.Image")));
+            this.buttonBrowseSource.Location = new System.Drawing.Point(208, 3);
+            this.buttonBrowseSource.Name = "buttonBrowseSource";
+            this.buttonBrowseSource.Size = new System.Drawing.Size(23, 23);
+            this.buttonBrowseSource.TabIndex = 1;
+            this.buttonBrowseSource.UseVisualStyleBackColor = true;
+            this.buttonBrowseSource.Click += new System.EventHandler(this.buttonBrowseSource_Click);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel3.Controls.Add(this.textBoxFileNameTarget, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.buttonBrowseTarget, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(563, 32);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(234, 362);
+            this.tableLayoutPanel3.TabIndex = 5;
+            // 
+            // textBoxFileNameTarget
+            // 
+            this.textBoxFileNameTarget.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxFileNameTarget.Location = new System.Drawing.Point(3, 3);
+            this.textBoxFileNameTarget.Name = "textBoxFileNameTarget";
+            this.textBoxFileNameTarget.Size = new System.Drawing.Size(199, 20);
+            this.textBoxFileNameTarget.TabIndex = 1;
+            this.textBoxFileNameTarget.Leave += new System.EventHandler(this.textBoxFileNameTarget_Leave);
+            // 
+            // buttonBrowseTarget
+            // 
+            this.buttonBrowseTarget.Image = ((System.Drawing.Image)(resources.GetObject("buttonBrowseTarget.Image")));
+            this.buttonBrowseTarget.Location = new System.Drawing.Point(208, 3);
+            this.buttonBrowseTarget.Name = "buttonBrowseTarget";
+            this.buttonBrowseTarget.Size = new System.Drawing.Size(23, 23);
+            this.buttonBrowseTarget.TabIndex = 2;
+            this.buttonBrowseTarget.UseVisualStyleBackColor = true;
+            this.buttonBrowseTarget.Click += new System.EventHandler(this.buttonBrowseTarget_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.mirrorsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportToolStripMenuItem,
+            this.importToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.exportToolStripMenuItem.Text = "Export";
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.importToolStripMenuItem.Text = "Import";
+            // 
+            // mirrorsToolStripMenuItem
+            // 
+            this.mirrorsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem,
+            this.removeToolStripMenuItem,
+            this.refreshToolStripMenuItem,
+            this.refreshAllToolStripMenuItem});
+            this.mirrorsToolStripMenuItem.Name = "mirrorsToolStripMenuItem";
+            this.mirrorsToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.mirrorsToolStripMenuItem.Text = "Mirrors";
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.addToolStripMenuItem.Text = "Add";
+            this.addToolStripMenuItem.Click += new System.EventHandler(this.AddMirror);
+            // 
+            // removeToolStripMenuItem
+            // 
+            this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.removeToolStripMenuItem.Text = "Remove";
+            this.removeToolStripMenuItem.Click += new System.EventHandler(this.RemoveMirror);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.RefreshMirror);
+            // 
+            // refreshAllToolStripMenuItem
+            // 
+            this.refreshAllToolStripMenuItem.Name = "refreshAllToolStripMenuItem";
+            this.refreshAllToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.refreshAllToolStripMenuItem.Text = "Refresh all";
+            this.refreshAllToolStripMenuItem.Click += new System.EventHandler(this.RefreshAllMirrors);
+            // 
+            // openFileDialogImport
+            // 
+            this.openFileDialogImport.FileName = "openFileDialog3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "File Mirror";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox textBoxFileNameSource;
+        private System.Windows.Forms.Button buttonBrowseSource;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TextBox textBoxFileNameTarget;
+        private System.Windows.Forms.Button buttonBrowseTarget;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mirrorsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshAllToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialogImport;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogExport;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogSource;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogTarget;
+    }
+}
+
---
+++ FileMirror/Form1.cs
@@ -0,0 +1,192 @@
+﻿using FileMirror.Properties;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Windows.Forms;
+
+namespace FileMirror
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Mirror> Mirrors = new List<Mirror>();
+        int Index = -1;
+
+        public void RefreshList(List<Mirror> mirrors, ListView listView)
+        {
+            listView.Items.Clear();
+            foreach (Mirror mirror in mirrors)
+            {
+                listView.Items.Add(mirror.Name);
+            }
+        }
+
+        public void ShowDetails(Mirror mirror)
+        {
+            textBoxName.Text = mirror.Name;
+            textBoxFileNameSource.Text = mirror.Source;
+            textBoxFileNameTarget.Text = mirror.Target;
+        }
+
+        public void ClearDetails()
+        {
+            textBoxName.Text = string.Empty;
+            textBoxFileNameSource.Text = string.Empty;
+            textBoxFileNameTarget.Text = string.Empty;
+
+        }
+
+        private void AddMirror(object sender, EventArgs e)
+        {
+            Mirror mirror = new Mirror();
+            Mirrors.Add(mirror);
+            RefreshList(Mirrors, listView1);
+        }
+
+        private void RemoveMirror(object sender, EventArgs e)
+        {
+            if (Index == -1)
+                return;
+            Mirrors.RemoveAt(Index);
+            RefreshList(Mirrors, listView1);
+        }
+
+        private void RefreshMirror(object sender, EventArgs e)
+        {
+            if (Index == -1)
+                return;
+            Mirrors[Index].Refresh();
+        }
+
+        private void RefreshAllMirrors(object sender, EventArgs e)
+        {
+            foreach (Mirror mirror in Mirrors)
+            {
+                mirror.Refresh();
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Index = -1;
+            ClearDetails();
+            if (listView1.SelectedIndices.Count > 0)
+            {
+                Index = listView1.SelectedIndices[0];
+                ShowDetails(Mirrors[Index]);
+            }
+        }
+
+        private void buttonBrowseSource_Click(object sender, EventArgs e)
+        {
+            if (Index == -1)
+                return;
+            if (folderBrowserDialogSource.ShowDialog() == DialogResult.OK)
+            {
+                Mirrors[Index].Source = folderBrowserDialogSource.SelectedPath;
+                ShowDetails(Mirrors[Index]);
+            }
+        }
+
+        private void buttonBrowseTarget_Click(object sender, EventArgs e)
+        {
+            if (Index == -1)
+                return;
+            if (folderBrowserDialogTarget.ShowDialog() == DialogResult.OK)
+            {
+                Mirrors[Index].Target = folderBrowserDialogTarget.SelectedPath;
+                ShowDetails(Mirrors[Index]);
+            }
+        }
+
+        private void textBoxName_Leave(object sender, EventArgs e)
+        {
+            if (Index == -1)
+                return;
+            Mirrors[Index].Name = textBoxName.Text;
+            RefreshList(Mirrors, listView1);
+        }
+
+        private void textBoxFileNameSource_Leave(object sender, EventArgs e)
+        {
+            if (Index == -1)
+                return;
+            Mirrors[Index].Source = textBoxFileNameSource.Text;
+        }
+
+        private void textBoxFileNameTarget_Leave(object sender, EventArgs e)
+        {
+            if (Index == -1)
+                return;
+            Mirrors[Index].Target = textBoxFileNameTarget.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Settings.Default.Mirrors != string.Empty)
+                Mirrors = JsonSerializer.Deserialize<List<Mirror>>(Settings.Default.Mirrors);
+            RefreshList(Mirrors, listView1);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Settings.Default.Mirrors = JsonSerializer.Serialize(Mirrors);
+            Settings.Default.Save();
+        }
+    }
+
+    public class Mirror
+    {
+        private static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+        {
+            var dir = new DirectoryInfo(sourceDir);
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            Directory.CreateDirectory(destinationDir);
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                string targetFilePath = Path.Combine(destinationDir, file.Name);
+                file.CopyTo(targetFilePath);
+            }
+            if (recursive)
+            {
+                foreach (DirectoryInfo subDir in dirs)
+                {
+                    string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                    CopyDirectory(subDir.FullName, newDestinationDir, true);
+                }
+            }
+        }
+
+        public Mirror(string Name = "Unnamed", string Source = "", string Target = "", bool SubDirectories = true)
+        {
+            this.Name = Name;
+            this.Source = Source;
+            this.Target = Target;
+            this.SubDirectories = SubDirectories;
+        }
+        public string Source { get; set; }
+        public string Target { get; set; }
+        public string Name { get; set; }
+        public bool SubDirectories { get; set; }
+        public Exception Refresh()
+        {
+            try
+            {
+                CopyDirectory(this.Source, this.Target, this.SubDirectories);
+            }
+            catch (Exception e)
+            {
+                return e;
+            }
+            return new Exception();
+        }
+    }
+
+}
